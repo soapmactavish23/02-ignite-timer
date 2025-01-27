85718434eeb96be510553c6421c8d046b7ed2236
Henrick <ricknogueira1231@gmail.com>
2025-01-27 14:56:22 -0300

    #D039 - Cores e fontes

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,10 +16,10 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  
-  background-color: ${props => props.theme.primary};
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-  /* ${(props) => {
-    return css`background-color: ${buttonVariants[props.variant]}`;
-  }} */
+  background-color: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme.white};
 `;
