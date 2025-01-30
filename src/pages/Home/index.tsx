35d0ccf3527f4f6e5551186a3d6adeeaeab88560
Henrick <ricknogueira1231@gmail.com>
2025-01-30 18:27:51 -0300

    #D046 - Aprimorando os inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,13 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from "./styles";
 
 export function Home() {
   return (
@@ -7,10 +15,26 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em </label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" list="task-suggestions" />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+            <option value="Projeto 4"></option>
+            <option value="Projeto 5"></option>
+            <option value="Banana"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount"  placeholder="00"/>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos.</span>
         </FormContainer>
