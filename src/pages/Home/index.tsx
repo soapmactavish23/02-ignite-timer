b1e1e9bee2db8cd929290fe329b922e4ac9d2b7a
Henrick <ricknogueira1231@gmail.com>
2025-02-02 19:16:16 -0300

    #D053 - Resetando Formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -30,7 +30,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
 export function Home() {
-  const { register, handleSubmit, watch, formState } =
+  const { register, handleSubmit, watch, formState, reset } =
     useForm<NewCycleFormData>({
       resolver: zodResolver(newCycleFormValidationSchema),
       defaultValues: { minutesAmount: 0, task: "" },
@@ -38,6 +38,7 @@ export function Home() {
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data);
+    reset();
   }
 
   const task = watch("task");
